a2b7a81e5e1b559561a063e5ccc9fb0c6b5981ff
pablo <devpabloh@gmail.com>
2025-05-29 11:01:01 -0300

    estruuturando projeto

---
+++ jogoAdivinhacao/src/utils/words.ts
@@ -0,0 +1,22 @@
+export type challenge = {
+    id: number,
+    word: string,
+    tip: string
+}
+
+export const WORDS: challenge[] = [
+    {id: 1, word: "CSS", tip: "Linguagem de estilos"},
+    {id: 2, word: "HTML", tip: "Linguagem de marcação"},
+    {id: 3, word: "JavaScript", tip: "Linguagem de programação"},
+    {id: 4, word: "React", tip: "Biblioteca JavaScript para interfaces web"},
+    {id: 5, word: "Node.js", tip: "Ambiente de execução JavaScript no servidor"},
+    {id: 6, word: "TypeScript", tip: "Superset do JavaScript com tipagem estática"},
+    {id: 7, word: "Express", tip: "Framework para Node.js"},
+    {id: 8, word: "MongoDB", tip: "Banco de dados NoSQL"},
+    {id: 9, word: "PostgreSQL", tip: "Banco de dados relacional"},
+    {id: 10, word: "Python", tip: "Linguagem de programação versátil"},
+    {id: 11, word: "Django", tip: "Framework web para Python"},
+    {id: 12, word: "Flask", tip: "Microframework para Python"},
+
+]
+
