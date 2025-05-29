de61fb8f16d90193ff59796ab8716d92d31ad938
pablo.henrique1 <pablo.henrique@ati.pe.gov.br>
2025-05-29 14:27:18 -0300

    Terminando a estrutura do header

--- jogoAdivinhacao/src/App.tsx
+++ jogoAdivinhacao/src/App.tsx
@@ -1,12 +1,17 @@
-import './App.css'
+import styles from './app.module.css'
+
+/* Components */
+import { Header } from './components/Header'
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <div className={styles.container}>
+      <main>
+        <Header/>
+      </main>
+    </div>
   )
 }
 
