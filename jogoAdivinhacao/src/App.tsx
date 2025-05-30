7e32d297f85f32a048acdf8f3b050d73fccd59b6
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-05-30 11:16:25 -0300

    feat: Criando componente de input no qual o usuário poderá colocar as tentativas das letras

--- jogoAdivinhacao/src/App.tsx
+++ jogoAdivinhacao/src/App.tsx
@@ -4,6 +4,7 @@ import styles from './app.module.css'
 import { Header } from './components/Header'
 import { Letters } from './components/Letters'
 import { Tip } from './components/Tip'
+import { Input } from './components/Input'
 
 function App() {
   function handleRestartGame (){
@@ -21,6 +22,10 @@ function App() {
           <Letters value="a"/>
           <Letters value="c"/>
           <Letters value="t"/>
+        </div>
+        <h4>Palpite</h4>
+        <div>
+          <Input autoFocus maxLength={1} placeholder='?'/>
         </div>
       </main>
     </div>
