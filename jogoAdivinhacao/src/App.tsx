672fe20d61a7e495e1741e4563512048b5096523
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-05-30 09:50:36 -0300

    feat: Criando o componente letter, que vai receber as letras e estilizando o main do arquivo app.tsx

--- jogoAdivinhacao/src/App.tsx
+++ jogoAdivinhacao/src/App.tsx
@@ -2,6 +2,7 @@ import styles from './app.module.css'
 
 /* Components */
 import { Header } from './components/Header'
+import { Letters } from './components/Letters'
 import { Tip } from './components/Tip'
 
 function App() {
@@ -14,6 +15,13 @@ function App() {
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame}/>
         <Tip tip='Linguagem de programação mais utilizadas'/>
+        <div className={styles.word}>
+          <Letters value="r"/>
+          <Letters value="e"/>
+          <Letters value="a"/>
+          <Letters value="c"/>
+          <Letters value="t"/>
+        </div>
       </main>
     </div>
   )
